3dfa816ed5bd19582fe2eeb1fbf8a88b1aa05549
MuhammadUsamaAwan <underusama@gmail.com>
2024-01-15 13:29:59 +0500

    first commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# Simple Wabbajack Modlist Downloader
+
+## Installation Steps
+
+1. Ensure [node.js](https://nodejs.org/) is installed on your system.
+1. Clone the [GitHub repository](https://github.com/muhammadusamaawan/wabbajack-modlist-downloader).
+1. Install all necessary packages with the command `npm i`.
+1. Rename `.env.example` to `.env`
+1. In the `.env` file, input your Nexus API key.
+1. Extract the desired `.wabbajack` file (change it to .zip and then extract).
+1. Within the extracted folder, locate the `modlist` file, rename it to `modlist.json`, and copy in this folder.
+1. Execute the program using `npm start`.
+
+Simplify your modlist downloading process with this straightforward Wabbajack modlist downloader.
---
+++ index.js
@@ -0,0 +1,36 @@
+import fs from 'fs';
+import axios from 'axios';
+
+const modlist = JSON.parse(fs.readFileSync('modlist.json'));
+
+const mods = modlist.Archives.filter((a) => a.State.$type === 'NexusDownloader, Wabbajack.Lib').map((a) => ({
+  name: a.Name,
+  gameName: a.State.GameName,
+  modId: a.State.ModID,
+  fileId: a.State.FileID,
+}));
+
+if (!fs.existsSync('mods')) {
+  fs.mkdirSync('mods');
+}
+
+for (const mod of mods) {
+  if (fs.existsSync(`mods/${mod.name}`)) {
+    console.log(`Skipping ${mod.name} as it already exists`);
+    continue;
+  }
+  console.log(`Downloading ${mod.name}`);
+  const downloadLinkResponse = await axios.get(
+    `https://api.nexusmods.com/v1/games/${mod.gameName}/mods/${mod.modId}/files/${mod.fileId}/download_link.json`,
+    {
+      headers: {
+        apikey: process.env.NEXUS_API_KEY,
+      },
+    }
+  );
+  const downloadLink = downloadLinkResponse.data[0].URI;
+  const modResponse = await axios.get(downloadLink, {
+    responseType: 'stream',
+  });
+  modResponse.data.pipe(fs.createWriteStream(`mods/${mod.name}`));
+}
